1ef6647f5d641546838bc17829d2fd6efe0e8a88
Ulik LOC-TAR <ulikloctar@gmail.com>
2026-04-30 00:41:34 +0300

    Fix game code generation to 5 characters

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const rooms = {};
 function generateCode() {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let code = '';
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 5; i++) {
     code += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return code;
